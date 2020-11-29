baf63bcb3ef2f9ea27d2195f5d67379becdb42e5
IkumaTadokoro <tadokorodev@gmail.com>
2020-11-29 10:13:00 +0900

    Githubでのログイン処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'welcome#index'
+  get "/auth/:provider/callback" => "sessions#create"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
