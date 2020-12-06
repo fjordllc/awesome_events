9eb7a1a9a64684ae9f09f26a4a76a4a78f1f119c
Chihiro <Chi.r.r.333@gmail.com>
2020-12-06 09:30:06 +0900

    ログアウト処理を追加した

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
+  delete "/logout" => "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
