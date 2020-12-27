567b7b05e9bf9a73aa2188e1151087001472ca56
IkumaTadokoro <tadokorodev@gmail.com>
2020-12-27 10:01:32 +0900

    イベント参加用モデルとコントローラを作成

---
+++ db/migrate/20201227000737_create_tickets.rb
@@ -0,0 +1,13 @@
+class CreateTickets < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tickets do |t|
+      t.references :user
+      t.references :event, null: false, foreign_key: true, index: false
+      t.string :comment
+
+      t.timestamps
+    end
+
+    add_index :tickets, %i[event_id user_id], unique: true
+  end
+end
