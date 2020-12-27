2da7943c43c7feb31dc1fe2f9d398fae3d93a331
IkumaTadokoro <tadokorodev@gmail.com>
2020-12-27 10:02:33 +0900

    イベント参加用ビューとルーティングの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  resources :events
   root 'welcome#index'
   get "/auth/:provider/callback" => "sessions#create"
   delete "/logout" => "sessions#destroy"
+
+  resources :events do
+    resources :tickets
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
