252c4572b3e9a2f61b1db142845657c2e3080171
Chihiro <Chi.r.r.333@gmail.com>
2021-02-14 09:12:05 +0900

    Event#created_by?メソッドのテストを追加

---
+++ test/models/event_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class EventTest < ActiveSupport::TestCase
+  test '#created_by? owner_idと引数のidが同じ時' do
+    event = FactoryBot.create(:event)
+    assert_equal(true, event.created_by?(event.owner))
+  end
+
+  test '#created_by? owner_idと引数のidが異なる時' do
+    event = FactoryBot.create(:event)
+    another_user = FactoryBot.create(:user)
+    assert_equal(false, event.created_by?(another_user))
+  end
+
+  test '#created_by? 引数がnilな時' do
+    event = FactoryBot.create(:event)
+    assert_equal(false, event.created_by?(nil))
+  end
+end
