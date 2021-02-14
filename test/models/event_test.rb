5190afaa3479a35e0cfcc85212c9b4d91550005d
Chihiro <Chi.r.r.333@gmail.com>
2021-02-14 09:23:28 +0900

    EventモデルテストのUserオブジェクトをスタブ化

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,7 +3,10 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   test '#created_by? owner_idと引数のidが同じ時' do
     event = FactoryBot.create(:event)
-    assert_equal(true, event.created_by?(event.owner))
+    user = User.new
+    user.stub(:id, event.owner_id) do
+      assert_equal(true, event.created_by?(user))
+    end
   end
 
   test '#created_by? owner_idと引数のidが異なる時' do
