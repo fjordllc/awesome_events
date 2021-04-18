4b3d3420f4e508d6fb6b83c3f2429dae65d9ddc0
FUGA0618 <aax.chiri@gmail.com>
2021-04-18 09:05:10 +0900

    404と500の場合のエラーハンドリングを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,18 @@ class ApplicationController < ActionController::Base
   before_action :authenticate
   helper_method :logged_in?, :current_user
 
+  rescue_from Exception, with: :error500
+  rescue_from ActiveRecord::RecordNotFound, ActionController::RoutingError, with: :error404
+
+  def error404(e)
+    render "error404", status: 404, formats: [:html]
+  end
+
+  def error500(e)
+    logger.error [e, *e.backtrace].join("\n")
+    render "error500", status: 500, formats: [:html]
+  end
+
   private
 
   def logged_in?
