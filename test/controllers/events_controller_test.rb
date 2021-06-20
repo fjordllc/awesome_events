fa20bd75ca4f3f74014ec36439c9bdec336dd2b0
Chihiro <Chi.r.r.333@gmail.com>
2021-06-20 11:55:11 +0900

    「他の人が作ったイベントは削除できない」のテストが落ちるので修正
    https://github.com/fjordllc/awesome_events/pull/37 4b3d342 にて、application_contorollerで、ActiveRecord::RecordNotFoundの例外をrescueし、404ページに飛ばすようにしたため、「ActiveRecord::RecordNotFound expected but nothing was raised.」のエラーがテストで発生してしまっていた。

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -16,9 +16,8 @@ class EventsControllerTest < ActionDispatch::IntegrationTest
     sign_in_user = FactoryBot.create(:user)
     sign_in_as sign_in_user
     assert_difference("Event.count", 0) do
-      assert_raises(ActiveRecord::RecordNotFound) do
-        delete event_url(event)
-      end
+      delete event_url(event)
     end
+    assert_response(:missing)
   end
 end
